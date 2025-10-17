0cbda34b1fa10d93964893dbe617b169ec6d13cc
MinhHung_T4TEK <dominhhung04032003@gmail.com>
2025-10-17 23:55:50 +0700

    first commit

---
+++ src/main/java/iuh/fit/se/acestore/AceStoreApplication.java
@@ -0,0 +1,13 @@
+package iuh.fit.se.acestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AceStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AceStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/iuh/fit/se/acestore/AceStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.fit.se.acestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AceStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
