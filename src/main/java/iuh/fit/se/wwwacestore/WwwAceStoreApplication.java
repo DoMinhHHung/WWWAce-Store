2d37bb4972484a638e7281eaf45a4055c616a89f
MinhHung_T4TEK <dominhhung04032003@gmail.com>
2025-10-19 07:59:56 +0700

    first commit

---
+++ src/main/java/iuh/fit/se/wwwacestore/WwwAceStoreApplication.java
@@ -0,0 +1,13 @@
+package iuh.fit.se.wwwacestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwwAceStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WwwAceStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/iuh/fit/se/wwwacestore/WwwAceStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.fit.se.wwwacestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwwAceStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
